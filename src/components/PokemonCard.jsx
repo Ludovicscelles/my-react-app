aad865eece7328d43e2dd3ecf2f21ee636518f6f
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-03-27 16:08:47 +0100

    Pokedex-Passer des props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,43 @@
-function PokemonCard () {
-    return <figure>
-    <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
-    </img>
-    <figcaption>
-    <p>Bulbasaur</p>
-    </figcaption>
-    </figure> ;
-  }
+function PokemonCard(props) {
+  const { pokemon } = props ;
+  return (
+    <section>
+      <img src = {pokemon.imgSrc}/>
+      <h1>{pokemon.name}</h1>
+    </section>
+  );
+}
+export default PokemonCard;
+
+// function PokemonCard() {
+//   const pokemon = { name: "mew" };
+
+//   console.log(props);
+//   console.log(pokemon.name);
+//   console.log(pokemon.imgSrc);
+
+//     if (!pokemon.imgSrc) {
+//       return (
+//         <>
+//           <p>???</p>
+//           <figcaption>{pokemon.name}</figcaption>
+//         </>
+//       );
+//     } else {
+//       return (
+//         <figure>
+//           <img src={pokemon.imgSrc} />
+//           <figcaption>{pokemon.name}</figcaption>
+//         </figure>
+//       );
+//       }
+//     }
+//   // // A faire pour la quête condition ternaire
+//   // return <p>...</p>;
+// // return <p>{!pokemon.imgSrc ? "???" + (pokemon.name) : (pokemon.imgSrc) + (pokemon.name) }</p> 
+ 
+// // }
+//   export default PokemonCard;
+
+
   
-  export default PokemonCard;
